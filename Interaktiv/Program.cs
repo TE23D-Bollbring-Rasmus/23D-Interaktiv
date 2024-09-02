f6e3c82a0b58581d7ec331a96b2090ac1fab62c3
Nils Rasmus Mattsson Bollbring <rasmus.mattssonbollbring@elev.ga.ntig.se>
2024-09-02 09:42:28 +0200

    Berättelse

--- Interaktiv/Program.cs
+++ Interaktiv/Program.cs
@@ -1,2 +1,37 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello World!");
+﻿Console.WriteLine("Hej! Är det varmt eller kallt? ");
+string temp = Console.ReadLine();
+
+if (temp.ToLower() == "kallt")
+{
+    Console.WriteLine("Vill du gå framåt eller åt höger?");
+    string direction1 = Console.ReadLine();
+    if (direction1.ToLower() == "framåt")
+    {
+        Console.WriteLine("Rätt!");
+        Console.ReadLine();
+    }
+    if (direction1.ToLower() == "höger")
+    {
+        Console.WriteLine("fel!!!!!!!!!!!!!");
+        Console.ReadLine();
+    }
+}
+
+if (temp.ToLower() == "varmt")
+{
+    Console.WriteLine("Vill du gå åt vänster eller bakåt?");
+    string direction2 = Console.ReadLine();
+    if (direction2.ToLower() == "vänster")
+    {
+        Console.WriteLine("Testa något annat");
+        Console.ReadLine();
+    }
+     if (direction2.ToLower() == "bakåt")
+     {
+        Console.WriteLine("Testa något annat");
+         Console.ReadLine();
+     }
+}
+
+
+Console.ReadLine();
